979d5e2bacaf9ab4c54d1547c6c3d8220e4268ee
Lysheedgc <dennigonzalez1211@gmail.com>
2022-06-04 17:22:13 -0500

    Tarea de Pregunta de opción múltiple (4/junio)

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -1,23 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public SpriteRenderer sprite;
-
+    public TextMeshProUGUI texto;
     // Start is called before the first frame update
     void Start()
     {
-        sprite.color = Color.green;
+        texto.text="a: ";
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            sprite.color = Color.blue;
+            texto.text="a: De México a Italia son 15 horas de vuelo en promedio.";
+        } else if (Input.GetKeyDown(KeyCode.B))
+        {
+            texto.text="a: De México a España son 11 horas 18 minutos de vuelo en promedio.";
+        } else if (Input.GetKeyDown(KeyCode.C))
+        {
+            texto.text="a: De México a Canadá son 4 horas 30 minutos de vuelo en promedio.";
+        } else if (Input.GetKeyDown(KeyCode.D))
+        {
+            texto.text="a: De México a Japón son 12 horas de vuelo en promedio.";
         }
     }
 }
